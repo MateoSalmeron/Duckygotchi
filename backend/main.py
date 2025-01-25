9dcf85964e9af3c44365ef4e5ce6458842c91862
MateoSalmeron <mateo.salmeron@gmail.com>
2025-01-25 12:40:26 +0100

    FEAT WIP add create duck structure

--- backend/main.py
+++ backend/main.py
@@ -20,9 +20,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/", StaticFiles(directory="../static", html=True), name="static")
 
 import json
+from duck.service import DuckService as DuckService
+
+
+duck_service = DuckService()
 
 #USER
 @app.post("/login")
@@ -42,14 +45,14 @@ def singup():
 def get_status():
     return "hello duck"
 
-@app.post("/duck/create")
-def create_duck():
-    return "hello duck"
+@app.post("/duck/create/{duck_name}")
+def create_duck(duck_name):
+    print(f"create Duck init: {duck_name}")
+    duck_service.create_duck(duck_name, None)
 
 @app.put("/duck/skin")
 def change_skin():
     return "hello duck"
-
 
 
 #Market
@@ -75,3 +78,49 @@ def buy_skin():
 if __name__ == '__main__':
   uvicorn.run("main:app",reload=True)
 
+
+#web socket
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+app.mount("/", StaticFiles(directory="../static", html=True, check_dir=False), name="static")
