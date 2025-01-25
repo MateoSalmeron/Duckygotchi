3a16ecc91ccc81e70722ba4b943cd8e762da3f44
Miguel Ángel Hita Acevedo <miguelangelhitaacev@gmail.com>
2025-01-25 17:33:41 +0100

    Feature: improving login on user.service

--- backend/user/service.py
+++ backend/user/service.py
@@ -1,14 +1,20 @@
 from user.User import User
+from user.repository import UserRepository
+from cookies.session import sessionData
 
 class UserService:
 
     def __init__(self):
         pass
 
-    def login (loginUser: User):
+    def login (self, loginUser: User):
         # call db to check if already exist
+        self.__private_user_exists(UserRepository.find_user_by_name(loginUser.name))
+        
         # if don't exists return false
         
+        # if exists generate session return session cookie
+        # return sessionData.cookie
         return False
 
     def logout():
@@ -19,4 +25,7 @@ class UserService:
         # if don't exist add to db
         # else return false
         return False
-
+    
+    def __private_user_exists(users: any):
+        for user in users:
+            print(user)
